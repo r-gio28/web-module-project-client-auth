779b9491d3420057e1a89e2aaee480e47d3f585a
Giovanni  Rosales <giovannirosales@Giovannis-MacBook-Pro.local>
2021-11-09 22:02:44 -0600

    working, forgot to push

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,17 +1,39 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 
 
 const Login = ()=> {
-  return (<h2>Login</h2>)
+  return (
+    <div className='login'>
+      <form>
+        <label>Username:
+          <input
+            type='text'
+          />
+        </label>
+        <label>Password:
+            <input
+              type='password'
+            />
+        </label>
+        <button>Login</button>
+      </form>
+    </div>
+  )
 }
 
 function App() {
   return (
+    <Router>
     <div className="App">
       <h2>Client Auth Project</h2>
     </div>
+    <Switch>
+      <Route path="/login" component={Login}/>
+      <Route path="/" component={Login}/>
+    </Switch>
+    </Router>
   );
 }
 
